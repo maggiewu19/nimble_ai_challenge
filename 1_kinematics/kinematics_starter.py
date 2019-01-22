6d0f72171c4e4961e8f85ccfd8e405518c80ae99
maggie <maggiewu@mit.edu>
2019-01-22 14:57:51 +0800

    kinematics

--- 1_kinematics/kinematics_starter.py
+++ 1_kinematics/kinematics_starter.py
@@ -18,7 +18,7 @@ class robot(object):
 		self.base_frame = base_frame
 		self.tool_transform = tool_transform
 
-		# ARM LENGTH (D-H) 
+		# arm length (d-h)
 		a_2 = -0.425
 		a_3 = -0.39225 
 		d_1 = 0.089159
@@ -28,6 +28,19 @@ class robot(object):
 		self.alpha_minus_1 = np.array([0, np.pi/2, 0, 0, np.pi/2, -np.pi/2])
 		self.a_minus_1 = np.array([0, 0, a_2, a_3, 0, 0])
 		self.d = np.array([d_1, 0, 0, d_4, d_5, d_6])
+
+	def transformation(t, alpha, a, d): 
+		''' To 
+			
+			input: t (theta) in radians 
+				   alpha from self.alpha_minus_1
+				   a from self.a_minus_1
+				   d from self.d 
+		'''
+		return m3d.Transform(np.array([[math.cos(t), -math.sin(t), 0, a], 
+										[math.sin(t)*math.cos(alpha), math.cos(t)*math.cos(alpha), -math.sin(alpha), -math.sin(alpha)*d], 
+										[math.sin(t)*math.sin(alpha), math.cos(t)*math.sin(alpha), math.cos(alpha), math.cos(alpha)*d], 
+										[0, 0, 0, 1]]))
 
 	# FORWARD KINEMATICS:
 	def getFK(self, joint_angles):
@@ -39,26 +52,17 @@ class robot(object):
 		tool_frame = m3d.Transform()
 		self.thetas = joint_angles 
 
-		def transformation(t, alpha, a, d): 
-			return m3d.Transform(np.array([[math.cos(t), -math.sin(t), 0, a], 
-											[math.sin(t)*math.cos(alpha), math.cos(t)*math.cos(alpha), -math.sin(alpha), -math.sin(alpha)*d], 
-											[math.sin(t)*math.sin(alpha), math.cos(t)*math.sin(alpha), math.cos(alpha), math.cos(alpha)*d], 
-											[0, 0, 0, 1]]))
-
 		t, alpha, a, d = self.thetas[0], self.alpha_minus_1[0], self.a_minus_1[0], self.d[0]
-		T_0_6 = transformation(t, alpha, a, d)
+		T_0_6 = self.transformation(t, alpha, a, d)
 
 		for i in range(1, 6): 
 			t, alpha, a, d = self.thetas[i], self.alpha_minus_1[i], self.a_minus_1[i], self.d[i]
-			T_0_6 *= transformation(t, alpha, a, d)
-
-		ee_transform = self.tool_transform.copy() 
-		T_0_6 *= ee_transform
+			T_0_6 *= self.transformation(t, alpha, a, d)
 
 		tool_frame.set_orient(T_0_6.get_orient())
 		tool_frame.set_pos(T_0_6.get_pos())
 
-		print (tool_frame)
+		print ('Forward Kinematics', tool_frame)
 
 		return tool_frame
 
@@ -119,11 +123,26 @@ class robot(object):
 		return jacobian
 
 
-	def getIK(self, poseGoal, seed_joint_angles=np.zeros((6,))):
+	# def getIK(self, poseGoal, seed_joint_angles=np.zeros((6,))):
+	# 	''' Analytically solve the inverse kinematics
+
+	# 		inputs: poseGoal = [x,y,z,rx,ry,rz] goal position of end-effector in global frame.
+	# 							Orientation [rx,ry,rz] specified in rotation vector (axis angle) form [rad]
+	# 				seed_joint_angles = [theta1, theta2, theta3, theta4, theta5, theta6]
+	# 									 seed angles for comparing solutions to ensure mode change does not occur
+
+	# 		outputs: joint_angles = joint angles in rad to reach goal end-effector pose
+	# 	'''
+
+	# 	joint_angles = np.zeros((1,6))
+	# 	####  YOUR CODE GOES HERE
+
+	# 	return joint_angles
+
+	def getIK(self, poseTransform, seed_joint_angles=np.zeros((6,))):
 		''' Analytically solve the inverse kinematics
 
-			inputs: poseGoal = [x,y,z,rx,ry,rz] goal position of end-effector in global frame.
-								Orientation [rx,ry,rz] specified in rotation vector (axis angle) form [rad]
+			inputs: poseTransform = 4 x 4 end effector transformation matrix
 					seed_joint_angles = [theta1, theta2, theta3, theta4, theta5, theta6]
 										 seed angles for comparing solutions to ensure mode change does not occur
 
@@ -131,10 +150,76 @@ class robot(object):
 		'''
 
 		joint_angles = np.zeros((1,6))
-		####  YOUR CODE GOES HERE
 
-		return joint_angles
+		print ('Pose Transform', poseTransform)
+		P_0_6 = poseTransform.get_pos()
+		P_0_6x = P_0_6[0]
+		P_0_6y = P_0_6[1]
 
+		O_0_6 = poseTransform.get_orient()
+		X_0_6x = O_0_6[0][0]
+		X_0_6y = O_0_6[1][0]
+		Y_0_6x = O_0_6[0][1]
+		Y_0_6y = O_0_6[1][1]
+
+		P_0_5 = poseTransform * m3d.Vector(np.array([0, 0, -self.d[5]]))
+		P_0_5x = P_0_5[0]
+		P_0_5y = P_0_5[1]
+
+		# theta 1 (2 possible solutions)
+		theta1 = [math.atan2(P_0_5y, P_0_5x) + math.acos(self.d[3]/math.sqrt(P_0_5x**2 + P_0_5y**2)) + np.pi/2, 
+				math.atan2(P_0_5y, P_0_5x) - math.acos(self.d[3]/math.sqrt(P_0_5x**2 + P_0_5y**2)) + np.pi/2]
+		print ('Theta 1', theta1)
+
+		# theta 5 (2 possible solutions)
+		theta5 = [(math.acos(P_0_6x * math.sin(theta1) - P_0_6y * math.cos(theta1) - self.d[3]))/self.d[5], 
+				-(math.acos(P_0_6x * math.sin(theta1) - P_0_6y * math.cos(theta1) - self.d[3]))/self.d[5]]
+		print ('Theta 5', theta5)
+
+		# theta 6 (1 possible solution)
+		theta6 = [math.atan2((-X_0_6y * math.sin(theta1) + Y_0_6y * math.cos(theta1))/math.sin(theta5), (X_0_6x * math.sin(theta1) - Y_0_6x * math.cos(theta1))/math.sin(theta5))]
+		print ('Theta 6', theta6)
+
+		# theta 3 (2 possible solutions)
+		rotation_0_1 = m3d.Orientation(np.array([[math.cos(theta1), math.sin(theta1), 0],
+												[-math.sin(theta1), math.cos(theta1), 0], 
+												[0, 0, 1]])) 
+		P_1_5 = rotation_0_1 * P_0_5 
+		T_5_6 = self.transformation(theta6, self.alpha_minus_1[5], self.a_minus_1[5], self.d[5])
+		T_4_5 = self.transformation(theta5, self.alpha_minus_1[4], self.a_minus_1[4], self.d[4])
+		T_0_1 = self.transformation(theta1, self.alpha_minus_1[0], self.a_minus_1[0], self.d[0])
+
+		T_1_4 = T_0_1.get_inverse() * poseTransform * T_5_6.get_inverse() * T_4_5.get_inverse() 
+
+		P_1_4 = T_1_4.get_pos() 
+		P_1_4x = P_1_4[0]
+		P_1_4y = P_1_4[1]
+		P_1_4z = P_1_4[2]
+
+		theta3 = [math.acos((P_1_4x**2+P_1_4z**2-self.a_minus_1[2]**2-self.a_minus_1[3]**2)/(2*self.a_minus_1[2]*self.a_minus_1[3])), 
+				-math.acos((P_1_4x**2+P_1_4z**2-self.a_minus_1[2]**2-self.a_minus_1[3]**2)/(2*self.a_minus_1[2]*self.a_minus_1[3]))]
+		print ('Theta 3', theta3)
+
+		# theta 2 (1 possible solution)
+		theta2 = [math.atan2(-P_1_4z, -P_1_4x)-math.asin(-self.a_minus_1[3]*math.sin(theta3)/math.sqrt(P_1_4x**2+P_1_4z**2))]
+		print ('Theta 2', theta2)
+
+		# theta 4 (1 possible solution)
+		T_1_2 = self.transformation(theta2, self.alpha_minus_1[1], self.a_minus_1[1], self.d[1])
+		T_2_3 = self.transformation(theta3, self.alpha_minus_1[2], self.a_minus_1[2], self.d[2])
+
+		T_3_4 = T_2_3.get_inverse() * T_1_2.get_inverse() * T_1_4 
+
+		O_3_4 = T_3_4.get_orient()
+		X_3_4x = O_3_4[0][0]
+		X_3_4y = O_3_4[1][0]
+
+		theta = [math.atan2(X_3_4y, X_3_4x)]
+		print ('Theta 4', theta4)
+
+		# select best solution out of valid thetas 
+
+		return joint_angles 
 
 
 	def xyzToolAngleError(self, joint_angles, poseGoal, angleErrorScale=.02):
@@ -174,6 +259,7 @@ tool_transform = m3d.Transform(np.array([[1, 0, 0, 0],
 										[0, 0, 1, 1],
 										[0, 0, 0, 1]])) 
 ur5 = robot(tool_transform=tool_transform) 
-ur5.getFK(np.array([np.pi/2, np.pi/2, 0, 0, np.pi/2, 0]))
+ur5fk = ur5.getFK(np.array([np.pi/2, np.pi/2, 0, 0, np.pi/2, 0]))
+ur5.getIK(ur5fk)
 
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nimble_ai_challenge
+## Link https://www.universal-robots.com/how-tos-and-faqs/faq/ur-faq/parameters-for-calculations-of-kinematics-and-dynamics-45257/
 Robotics Code Challenge
